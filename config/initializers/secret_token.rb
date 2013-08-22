36d1b942cf28b3d6a7dc6c343ecf71b43446271b
Seth B. Altshuler <sethb78@gmail.com>
2013-08-22 17:01:26 -0400

    first commit header

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suzanne::Application.config.secret_key_base = '6615c5c1a4dff55c066d782c790f1142b6f5161cb9fcf1e4e4acc668a3007ec80f999c15f7c83d07d59c2d76171b2a69260a6020e190b3a0a84cae021fa5f4e3'
