ff2cf42f662f518a36a441668a631b0b1a09c5ad
Seth B. Altshuler <sethb78@gmail.com>
2013-09-06 16:26:45 -0400

    clean dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,80 +3,72 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
+  #   div :class => "blank_slate_container", :id => "dashboard_default_message" do
+  #     span :class => "blank_slate" do
+  #       span I18n.t("active_admin.dashboard_welcome.welcome")
+  #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #     end
+  #   end
 
-  section  link_to "Edit Personal Info", edit_admin_resume_personal_path(ResumePersonal.last.id) do
-    table_for ResumePersonal.all do
-    column :city
-    column :state
-    column :zip
-    column :cell_phone
-    column :email
-    column :linkedin
+  div :class => "twenty_five_percent float-left" do
+    panel "Admin Users" do
+      ul do
+        AdminUser.all.each do |user|
+          li user.email
+        end
+        strong link_to("Admin Users Page", admin_admin_users_path)
+      end
     end
-end
+  end
 
-  section link_to "Edit Summary Text", edit_admin_summary_text_path(SummaryText.last.id) do
-    table_for SummaryText.all do
-      column :summary 
+  div :class => "float-left" do
+    panel "Resume Uploads" do
+      ul do
+          li link_to("edit #{Resume.last.document}", admin_resume_path(Resume.last))
+        
+      end
     end
   end
 
-  section link_to "Summary Bullet Points", admin_summary_bullet_points_path do
-  table_for SummaryBulletPoint.all.each do
-    column :feature do |feature|
-        link_to feature.feature, edit_admin_summary_bullet_point_path(feature)
-      end
-  end
-end
-
-  section link_to "Professional Expereince", admin_professional_experiences_path do
-  table_for ProfessionalExperience.all.each do
-    column :title do |job|
-        link_to job.title, edit_admin_professional_experience_path(job)
-      end
-  end
-end
-
-  section link_to "Leadership and Experience Awards", admin_leadership_experience_and_awards_path do
-  table_for LeadershipExperienceAndAward.all.each do
-    column :title do |entry|
-        link_to entry.title, edit_admin_leadership_experience_and_award_path(entry)
-      end
-  end
-end
-
-section link_to "Education", admin_educations_path do
-  table_for Education.all.each do
-    column :description do |entry|
-        link_to entry.description, edit_admin_education_path(entry)
-      end
-  end
-end
-
-section link_to "Professional Development", admin_professional_developments_path do
-  table_for ProfessionalDevelopment.all.each do
-    column :title do |entry|
-        link_to entry.title, edit_admin_professional_development_path(entry)
-      end
-  end
-end
-
-  section "Recent Blogs" do
-    table_for Blog.order("updated_at desc").limit(5) do
-      column :name do |blog|
-        link_to blog.title, admin_blog_path(blog)
-      end
-      column :updated_at
+  panel "User Updated Resume" do
+    ul do
+      li link_to(" edit Personal Information", edit_admin_resume_personal_path(ResumePersonal.last))
+      li link_to("edit Summary Text", edit_admin_summary_text_path(SummaryText.last))
+      li link_to("edit Summary Bullet Points", admin_summary_bullet_points_path)
+      li link_to("edit Professional Experience", admin_professional_experiences_path)
+      li link_to("edit Leadership Experience & Awards", admin_leadership_experience_and_awards_path)
+      li link_to("edit Education", admin_educations_path)
+      li link_to("edit Professional Development", admin_professional_developments_path)
+      li link_to("edit Community Activities", admin_community_activities_path)
     end
-    strong { link_to "View All Blogs", admin_blogs_path }
-       div :class => "clearfix"
-        strong { link_to "Add Blog", new_admin_blog_path}
-
-
   end
 
+  panel "Blogs" do
+    ul do
+      Blog.all.each do |blog|
+        li link_to(blog.title, edit_admin_blog_path(blog))
+      end
+        strong link_to("Blogs Index", admin_blogs_path)
+    end
+  end
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    # columns do
+    # column do
+    # panel "Recent Posts" do
+    # ul do
+    # Post.recent(5).map do |post|
+    # li link_to(post.title, admin_post_path(post))
+    # end
+    # end
+    # end
+    # end
 
-
-
+    # column do
+    # panel "Info" do
+    # para "Welcome to ActiveAdmin."
+    # end
+    # end
+    # end
   end # content
 end
