a212be42df9de721f03712542a3db456dfd66252
Seth B. Altshuler <sethb78@gmail.com>
2013-09-05 20:23:52 -0400

    update

--- config/initializers/active_admin_editor.rb
+++ config/initializers/active_admin_editor.rb
@@ -1,6 +1,6 @@
-ActiveAdmin::Editor.configure do |config|
-  # config.s3_bucket = ''
-  # config.aws_access_key_id = ''
-  # config.aws_access_secret = ''
-  # config.storage_dir = 'uploads'
-end
+# ActiveAdmin::Editor.configure do |config|
+#   # config.s3_bucket = ''
+#   # config.aws_access_key_id = ''
+#   # config.aws_access_secret = ''
+#   # config.storage_dir = 'uploads'
+# end
